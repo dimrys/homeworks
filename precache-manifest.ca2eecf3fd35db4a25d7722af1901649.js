988f588f7f51ebacb069ee1fdfd78eba3445544e
dimrys <akkauntdlyraboti@gmail.com>
2021-02-17 16:26:47 +0300

    Updates

---
+++ precache-manifest.ca2eecf3fd35db4a25d7722af1901649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b55f374d979fd4a03477cfd9ef08a0b",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "2e13acb4b8cd388ff1f4",
+    "url": "/homeworks/static/css/main.bd127b23.chunk.css"
+  },
+  {
+    "revision": "3ebe1466e1ae9c469971",
+    "url": "/homeworks/static/js/2.602c5356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworks/static/js/2.602c5356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e13acb4b8cd388ff1f4",
+    "url": "/homeworks/static/js/main.d636653b.chunk.js"
+  },
+  {
+    "revision": "9263b9c20382da27b976",
+    "url": "/homeworks/static/js/runtime-main.1f7d4fd3.js"
+  }
+]);
