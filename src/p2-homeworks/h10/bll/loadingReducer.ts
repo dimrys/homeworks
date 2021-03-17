1e9e7bcfd6f542c23e18943655cd5a2c63116c0a
dimrys <akkauntdlyraboti@gmail.com>
2021-03-17 09:41:25 +0300

    completed the task 10 (logics)

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,36 @@
-const initState = {
+enum ActionType  {
+    CHANGE_LOADING = 'CHANGE-LOADING'
+}
 
+
+type InitStateType = {
+    loading: boolean
+}
+type LoadingType = {
+    type: ActionType.CHANGE_LOADING
+    loading: boolean
+}
+
+
+const initState = {
+    loading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state:InitStateType = initState, action: LoadingType): InitStateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case ActionType.CHANGE_LOADING: {
+            return {
+                ...state,
+                loading: action.loading
+            };
         }
         default: return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (loading: boolean): LoadingType => {
+    return {
+        type:ActionType.CHANGE_LOADING,
+        loading
+    }
+}; // fix any
